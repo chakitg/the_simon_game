52de83e58101a457e3c0e524f2edc3aa080f9c23
Chakit Gupta <chakitg1@gmail.com>
2023-08-05 00:03:55 +0530

    step4

--- game.js
+++ game.js
@@ -1,19 +1,29 @@
-//3. At the top of the game.js file, create a new array called buttonColours and set it to hold the sequence "red", "blue", "green", "yellow" .
+
 var buttonColours = ["red", "blue", "green", "yellow"];
 
-//5. At the top of the game.js file, create a new empty array called gamePattern.
 var gamePattern = [];
 
-//1. Inside game.js create a new function called nextSequence()
+var userClickedPattern = [];
+
+$(".btn").click(function() {
+
+    var userChosenColour = $(this).attr("id");
+  
+    userClickedPattern.push(userChosenColour);
+  
+    // console.log(userClickedPattern);
+  
+  });
+
+
 function nextSequence() {
 
-  //2. Inside the new function generate a new random number between 0 and 3, and store it in a variable called randomNumber
-  var randomNumber = Math.floor(Math.random() * 4);
-
-  //4. Create a new variable called randomChosenColour and use the randomNumber from step 2 to select a random colour from the buttonColours array.
-  var randomChosenColour = buttonColours[randomNumber];
-
-  //6. Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
-  gamePattern.push(randomChosenColour);
-
-}
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+  
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+  }
